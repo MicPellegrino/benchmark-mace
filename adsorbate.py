1433420ed039b9b095c4225a77576ed31817450a
Michele <michele.pellegrino@scilifelab.se>
2025-04-01 19:26:32 +0200

    Water adsorbed on FCC Al

---
+++ adsorbate.py
@@ -0,0 +1,31 @@
+from mace.calculators import mace_mp
+from mace.calculators import MACECalculator
+
+from ase import build
+from ase.md import Langevin
+from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
+from ase import units
+from ase.build import fcc111, add_adsorbate
+
+from testmd import test_md
+
+model_name = "OMAT_medium_foundation.model"
+print("##### Testing",model_name,"#####")
+
+# macemp = mace_mp(model="medium-mpa-0", device='cuda', enable_cueq=True, default_dtype="float64")
+macemp = MACECalculator(model_paths="models-uob/GPU_Models/BCC_Foundation_Model/"+model_name, device='cuda', enable_cueq=True, default_dtype="float64")
+
+# Water molecule on a FCC Al slab
+slab = fcc111('Al', size=(2,2,3))
+add_adsorbate(slab, 'H2O', 1.5, 'ontop')
+
+slab.calc = macemp
+
+# Set up the Langevin dynamics engine for NVT ensemble.
+dyn = Langevin(slab, 0.5*units.fs, temperature_K=300, friction=0.01/units.fs, logfile='water.log')
+
+time, temperature, energies, _, _ = test_md(slab, macemp, dyn, nsteps=200, fname='water.xyz', ndump=10, seed=1234, T=300)
+
+print("time =",time)
+print("T =",temperature)
+print("e =", energies)
